61d939b9575c6adc42bf66cf32a799ba64c4e00b
Tech AJ <32822650+halanch599@users.noreply.github.com>
2021-03-02 09:22:10 +0300

    Update taylorexpension.m

--- taylorexpension.m
+++ taylorexpension.m
@@ -1,43 +1,34 @@
+% Date: 02.03.2021
+% Author: Dr. Akhtar Jamil
 clc;
 clear;
 close all;
-x=-3:.1:3;
-y=exp(x);
 
-figure,
-plot(x,y,'r-','LineWidth',2);
+x=-3:0.1:3;
+f = @(x)exp(x);
+y = f(x);
+
+figure
+
+plot(x,y,'r--','LineWidth',2);
 xlabel('X')
-ylabel('Y')
-grid on; 
-hold on
-
-%% Taylor Series Expension
+ylabel('Y');
 
 y_predict = zeros(size(y));
+
+N=10;
+
 for n=0:N
-    y_predict = y_predict+(x.^n)./factorial(n); 
+    y_predict = y_predict+ (x.^n)./factorial(n);
 end
+
 hold on
-
-tv_total = sum(y);
-ev_total = sum(y_predict);
-total_error = abs((tv_total-ev_total)/tv_total)*100;
-
-N=1;
-ev=0;
-xx=1;
-for n=0:N
-    ev = ev+(xx^n)./factorial(n); 
-end
-tv=exp(xx);
-err = (tv-ev)/tv*100;
-
-msg = sprintf('Error = %f Total error = %f',err, total_error);
-disp(msg)
-% title(['Error = ',num2str(err),'%'])
+plot(x,y_predict,'b','LineWidth',2);
+err = abs((y - y_predict)./y);
+msg =sprintf('Error = %f',sum(err));
 title(msg)
-plot(x,y_predict,'b--','LineWidth',2)
-plot(xx,tv,'or','MarkerSize',10,'LineWidth',2)
-plot(xx,ev,'og','MarkerSize',10,'LineWidth',2)
-
 legend('True Value','Estimated Value')
+grid
+
+
+
